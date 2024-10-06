3eae562d6fbfb873d858dd42db1b7aad0084535d
wiacze <151467867+wiacze@users.noreply.github.com>
2024-10-06 13:08:22 +0300

    Update settings.py

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'token')
 
-DEBUG = os.getenv('DEBUG', False)
+DEBUG = bool(os.getenv('DEBUG') == 'True')
 
 SERVER_IP = os.getenv('SERVER_IP', '127.0.0.1')
 DOMAIN = os.getenv('DOMAIN', 'localhost')
